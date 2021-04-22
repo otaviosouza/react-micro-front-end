312ee0eba3e99d5fd5e3d993514dae1b84742f72
Otavio Souza <otaviojr.work@gmail.com>
2021-04-22 14:24:53 -0300

    update(Contact): Contact page creation

---
+++ packages/contact/src/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// CSS
+import './contact.css'
+
+const Contact = () => {
+    return (
+        <div id="contact-page">
+            <h1>Contact Page</h1>
+            <p>This is a simple Contact Page</p>
+        </div>
+    )
+}
+
+export default Contact
---
+++ packages/contact/src/bootstrap.js
@@ -0,0 +1,7 @@
+//this file starts the app
+import Contact from './Contact'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+ReactDOM.render(<Contact />, document.querySelector('#root'))
